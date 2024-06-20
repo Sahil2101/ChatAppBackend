5cfb3420a519a8045ab71916eafb6dfccae6e35f
Sahil2101 <sahiljalal21@gmail.com>
2024-06-20 11:26:57 +0530

    fixing cors

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const server = app.listen(process.env.PORT,()=>{
 
 const io =socket(server,{
     cors:{
-        origin:"process.env.FRONTEND_URL",
+        origin: process.env.FRONTEND_URL,
         credential:"true",
     },
 });
